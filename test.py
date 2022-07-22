7bf0b4b9c07b98ade51fb1ed5510238a34ae0903
david-dev-9 <105790977+david-dev-9@users.noreply.github.com>
2022-07-22 03:55:36 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,10 @@
 x = int (input ("Number: "))
 
-if x > 2:
+if x > 0:
     print("Number is +")
 
 elif x == 0:
     print("Number is zero")
-
-elif x == 1 :
-    print("Number is 1")
-
-
-elif x == 2 :
-    print("Number is 2")
-
 
 else :
     print("Number is -")
